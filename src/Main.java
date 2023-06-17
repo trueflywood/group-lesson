9b69022ed69bdb809b031efa9845059ad4deddc6
Overlord7667 <aspiaro@gmail.com>
2023-06-17 11:59:36 +0300

    add list

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 class Person{
     private String name;
     private String surname;
@@ -73,7 +78,27 @@ class Person{
 }
 public class Main {
     public static void main(String[] args) {
-        
+        int select = 0;
+        Scanner scanner;
+        List<Person> list = new ArrayList<>();
+        if (select == 0){
+            scanner =  new Scanner(System.in);
+            System.out.println("Create");
+            System.out.println("Enter name : ");
+            String name = scanner.nextLine();
+            System.out.println("Enter surname : ");
+            String surname = scanner.nextLine();
+            System.out.println("Enter phone : ");
+            String phone = scanner.next();
+            System.out.println("Enter gender : ");
+            String gender = scanner.next();
+            System.out.println("Enter age : ");
+            int age = scanner.nextInt();
+            System.out.println("Enter type : ");
+            String type = scanner.next();
+            Person person = new Person(name,surname,phone, gender, age, type);
+            list.add(person);
+        }
     }
 }
 
