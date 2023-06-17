fc76c42c720cb9b9a7233fe69aa1ac2d4974d1e2
Overlord7667 <aspiaro@gmail.com>
2023-06-17 10:27:40 +0300

    add person

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+class Person{
+    private String name;
+    private String surname;
+    private String phone;
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("");
     }
 }
