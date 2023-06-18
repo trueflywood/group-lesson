64bad57e66d282c73b57a1d4256472d5bf5a6d72
Алексей Компанец <trueflywood@gmail.com>
2023-06-18 10:39:33 +0300

    вывод списка

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         HashSet<Person> people = new HashSet<Person>();
-        people.contains(new Person("Такеши", "Китано","88005553535","Мужчина", 76,"Брат"));
-        people.contains(new Person("Алексей", "Абрамович","3743437863478","Мужчина", 63,"Дальний родственник"));
-        people.contains(new Person("Аяко", "Саичиро","478546567878","Женщина", 32,"Супруга"));
-        people.contains(new Person("Милана", "Янковки","56474637656","Женщина", 35,"Сестра"));
+        people.add(new Male("Такеши", "Китано","88005553535", 76,"Брат"));
+        people.add(new Male("Алексей", "Абрамович","3743437863478", 63,"Дальний родственник"));
+        people.add(new Female("Аяко", "Саичиро","478546567878",32,"Супруга"));
+        people.add(new Female("Милана", "Янковки","56474637656", 35,"Сестра"));
+
+        System.out.println(people);
     }
 }
 
