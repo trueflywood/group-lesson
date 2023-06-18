a54fcd06770a060c72c1cc2ba3f4b050cfcc7886
Алексей Компанец <trueflywood@gmail.com>
2023-06-18 10:21:01 +0300

    классы мужчины и женщины с перегрузкой методов

--- src/Male.java
+++ src/Male.java
@@ -1,5 +1,41 @@
+import java.util.List;
+import java.util.Objects;
+
 public class Male extends Person {
-    public Male(String name, String surname, String phone, String gender, int age, String type) {
-        super(name, surname, phone, gender, age, type);
+
+    final Gender gender = Gender.MALE;
+
+    private List<MaleConnections> connections;
+    public Male(String name, String surname, String phone, int age, String type) {
+        super(name, surname, phone, age, type);
     }
+
+    public List<MaleConnections> getConnections() {
+        return connections;
+    }
+
+    public void setConnections(List<MaleConnections> connections) {
+        this.connections = connections;
+    }
+
+   @Override
+    public String toString() {
+        return super.toString()+  "=" + gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Male male)) return false;
+        if (!super.equals(o)) return false;
+        return gender == male.gender;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), gender);
+    }
+
+
+
 }
